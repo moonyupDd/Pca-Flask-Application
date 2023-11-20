d90295042597fe54a0a939ddb15d43184603ca0b
duy <duy27pham09@gmail.com>
2023-11-20 09:56:11 +0700

    add graph

--- pca_application.py
+++ pca_application.py
@@ -32,7 +32,7 @@ def test_pca():
     img = img.astype(np.uint8)
     img = img / 255
     img_gray = np.mean(img, axis=2)  # Convert the image to grayscale
-    plt.imshow(img_gray, cmap="gray")
+    # plt.imshow(img_gray, cmap="gray")
 
     # Reshape the image to a 2D array
     img_2d = img_gray.reshape(-1, img_gray.shape[1])
@@ -44,20 +44,79 @@ def test_pca():
 
     # Reshape the projection image back to the original shape
     projection = projection.reshape(img_gray.shape)
-    # Create the "image" folder if it doesn't exist
+
+    # Create graph
+    explained_variance_ratio = pca.explained_variance_ratio_
+    num_components = len(explained_variance_ratio)
+    components = range(1, num_components + 1)
+    plt.figure()
+    plt.plot(components, explained_variance_ratio, "bo-")
+    plt.xlabel("Principal Component")
+    plt.ylabel("Explained Variance Ratio")
+    plt.title("PCA Explained Variance Ratio")
+    if not os.path.exists("static/graphs"):
+        os.makedirs("static/graphs")
+
+    # Get the number of files already in the folder
+    file_graph_count = len(os.listdir("static/graphs"))
+
+    # Save the image with an auto-incremented name
+    graphname = f"static/graphs/graph_{file_graph_count + 1}.png"
+    graph_url = f"../{graphname}"  # Construct the URL for the graph
+
+    plt.savefig(graphname)
+
+    # Create the "image" , "graph" folder if it doesn't exist
     if not os.path.exists("static/images"):
         os.makedirs("static/images")
 
+    if not os.path.exists("static/graphs"):
+        os.makedirs("static/graphs")
+
     # Get the number of files already in the folder
     file_count = len(os.listdir("static/images"))
+    file_graph_count = len(os.listdir("static/graphs"))
 
     # Save the image with an auto-incremented name
     filename = f"static/images/image_{file_count + 1}.png"
     plt.imsave(filename, projection, cmap="gray")
 
+    # Save the graph with an auto-incremented name
+    graphname = f"static/graphs/graph_{file_graph_count + 1}.png"
+    graph_url = f"../{graphname}"  # Construct the URL for the graph
+    plt.savefig(graphname)
+
     image_url = f"../{filename}"  # Construct the URL for the image
 
-    return jsonify({"image_url": image_url, "combined_word": perct})
+    return jsonify(
+        {"image_url": image_url, "combined_word": perct, "graph_url": graph_url}
+    )
+
+
+# @app.route("/graph")
+# def test_graph(pca):
+# Create graph
+# explained_variance_ratio = pca.explained_variance_ratio_
+# num_components = len(explained_variance_ratio)
+# components = range(1, num_components + 1)
+# plt.figure()
+# plt.plot(components, explained_variance_ratio, "bo-")
+# plt.xlabel("Principal Component")
+# plt.ylabel("Explained Variance Ratio")
+# plt.title("PCA Explained Variance Ratio")
+
+# if not os.path.exists("static/graphs"):
+#     os.makedirs("static/graphs")
+
+# Get the number of files already in the folder
+# file_graph_count = len(os.listdir("static/graphs"))
+
+# Save the image with an auto-incremented name
+# graphname = f"static/graphs/graph_{file_graph_count + 1}.png"
+# graph_url = f"../{graphname}"  # Construct the URL for the graph
+# plt.savefig(graphname)
+
+# return jsonify({"graph_url": graph_url})
 
 
 if __name__ == "__main__":
